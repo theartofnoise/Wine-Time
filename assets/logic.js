3a1782000e4f5180cc8590a8224a102a32465c23
Art G <theartofnoise@gmail.com>
2019-02-02 12:55:29 -0500

    new colors for bad wine

--- assets/logic.js
+++ assets/logic.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
     // Initialize Firebase
@@ -12,6 +13,20 @@ $(document).ready(function(){
   firebase.initializeApp(config);
 
   var db = firebase.database().ref();
+
+  //Submit input validation--------------------------------
+// var submit = $("#submitButtonTypes");
+// var beerType = $("#userInputTypes");
+
+// beerType.on('keyup', function (event) {
+//     isValidType = beerType[0].checkValidity();    
+    
+//     if ( isValidType ) {
+//         submit.attr("disabled",false);
+//     } else {
+//         submit.attr("disabled",true);
+//     }
+//   });
 
   $("#submit").on("click", function (event) {
     event.preventDefault();
@@ -37,6 +52,7 @@ $(document).ready(function(){
     }
 
     // push to database
+    
     db.push(newWine);
 
     //erase input values
@@ -48,17 +64,31 @@ $(document).ready(function(){
     julie.prop('selectedIndex',0);
     price.val("");
     
-
+    
 });
 
 //submit button
 db.on("child_added", function (childSnap) {
        
-
+    
     //add values to html
-    var newRow = "<tr><td>" + childSnap.val().name + "</td><td>" + childSnap.val().type + "</td><td>" + childSnap.val().year + "</td><td>" + childSnap.val().description + "</td><td>" + childSnap.val().arthur + "</td><td>" + childSnap.val().julie + "</td><td>" + childSnap.val().price + "</td></tr>"
+    var newRow = "<tr><td><strong>" + childSnap.val().name + "</strong></td><td>" + childSnap.val().type + "</td><td>" + childSnap.val().year + "</td><td>" + childSnap.val().description + "</td><td>" + childSnap.val().arthur + "</td><td>" + childSnap.val().julie + "</td><td>" + childSnap.val().price + "</td></tr>"
+    var newBadRow = "<tr class='nameHighlight'><td class='smallNameHighlight'><strong>" + childSnap.val().name + "</strong></td><td>" + childSnap.val().type + "</td><td>" + childSnap.val().year + "</td><td>" + childSnap.val().description + "</td><td>" + childSnap.val().arthur + "</td><td>" + childSnap.val().julie + "</td><td>" + childSnap.val().price + "</td></tr>"
+    var badArtRow = "<tr class='artHighlight'><td class='smallArtHighlight'><strong>" + childSnap.val().name + "</strong></td><td>" + childSnap.val().type + "</td><td>" + childSnap.val().year + "</td><td>" + childSnap.val().description + "</td><td>" + childSnap.val().arthur + "</td><td>" + childSnap.val().julie + "</td><td>" + childSnap.val().price + "</td></tr>"
+    var badJulieRow = "<tr class='julieHighlight'><td class='smallJulieHighlight'><strong>" + childSnap.val().name + "</strong></td><td>" + childSnap.val().type + "</td><td>" + childSnap.val().year + "</td><td>" + childSnap.val().description + "</td><td>" + childSnap.val().arthur + "</td><td>" + childSnap.val().julie + "</td><td>" + childSnap.val().price + "</td></tr>"
+    
+    if (childSnap.val().arthur === "No" && childSnap.val().julie === "No") {
+        console.log("arthur or julie no");
+        // $(".nameHighlight").css("color","red");
+        $('#topRow').append(newBadRow);
+    } else if (childSnap.val().arthur === "No" && childSnap.val().julie === "Yes") {
+        $('#topRow').append(badArtRow); 
+    } else if (childSnap.val().arthur === "Yes" && childSnap.val().julie === "No") {
+        $('#topRow').append(badJulieRow); 
+    } else {
     $('#topRow').append(newRow);
-
+    }
+    
 }, function (errorObject) {
     console.log('Error: ' + errorObject);
 });
